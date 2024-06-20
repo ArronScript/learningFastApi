6391b4b9f72cfd53891689023fbf7d2763889a1c
ivanz ArronLovely <ivanchexa@gmail.com>
2024-06-20 11:42:03 +0300

    After jwt

--- backend/api_1/security/crud.py
+++ backend/api_1/security/crud.py
@@ -1,17 +1,44 @@
-from sqlalchemy import select
+import base64
+
+from typing import Tuple, Type
+
+from sqlalchemy import select, Result
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from backend.core.models import User
 
-
 from .shemas import UserCreate
 
 
+class PasswordCoding:
+    @staticmethod
+    def encode_password(password: str) -> str:
+        return base64.b64encode(password.encode()).decode()
 
-async def add_user(session: AsyncSession, user_in: UserCreate) -> User | None:
-    print({**user_in.model_dump()})
+    @staticmethod
+    def decode_password(encoded_password: str) -> str:
+        return base64.b64decode(encoded_password.encode()).decode()
 
-    user = User(**user_in.model_dump())
-    session.add(user)
-    await session.commit()
-    return user
+
+class DatabaseUser:
+    @staticmethod
+    async def add_user(session: AsyncSession, user_in: UserCreate) -> User | None:
+        user_in.password = PasswordCoding.encode_password(user_in.password)
+        print({**user_in.model_dump()})
+        user = User(**user_in.model_dump())
+        print(user.__dict__)
+        session.add(user)
+        await session.commit()
+        return user
+
+# -> Type[User] | None
+    @staticmethod
+    async def get_user(session: AsyncSession, login: str, password: str):
+        statement = select(User).order_by(User.username)
+        result: Result = await session.execute(statement)
+
+        print(result.all())
+
+
+
+
--- backend/api_1/security/views.py
+++ backend/api_1/security/views.py
@@ -3,16 +3,16 @@ from fastapi.security import OAuth2PasswordBearer
 
 from passlib.context import CryptContext
 
-from backend.api_1.security import User, UserCreate
-from backend.api_1.security import create_jwt_token
+from backend.api_1.security import UserCreate
 from backend.core.models import db_helper
 from sqlalchemy.ext.asyncio import AsyncSession
 
-
-from .crud import add_user
-
+from .crud import DatabaseUser
 
 from backend.core.models import db_helper
+
+from backend.core.models import User
+
 security_router = APIRouter(prefix='/demo-auth', tags=['Demo Auth'])
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
@@ -24,33 +24,25 @@ async def register_user(username: str,
                         password: str,
                         email: str | None = None,
                         full_name: str | None = None,
-                        disabled: bool | None = None,
                         session: AsyncSession = Depends(db_helper.scoped_session_dependency)
                         ):
-    user_in = UserCreate(username=username, password=password, hash_password='None',
-                         email=email, full_name=full_name,disabled=disabled)
-
-    user_in.hash_password = str(pwd_context.hash(password))
-    await add_user(session, user_in)
-    # Сохраните пользователя в базе данных
-
-    return {"user": user_in.hash_password}
-
-
-async def get_user(username):
-    return User(username= "Nika",password= '123qwe123qwe', email='nika@mail.ru', )
+    user_in = UserCreate(username=username, password=password,
+                         email=email, full_name=full_name)
+    print(user_in.__dict__)
+    await DatabaseUser.add_user(session, user_in)
+    return {"user": user_in.password}
 
 
 @security_router.post("/token")
-async def authenticate_user(username: str, password: str):
-    user = get_user(username)  # Получите пользователя из базы данных
+async def authenticate_user(username: str, password: str,
+                            session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
+    user = await DatabaseUser.get_user(session=session, login=username, password=password)
+
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    is_password_correct = pwd_context.verify(password, user.hash_password)
-
-    if not is_password_correct:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    jwt_token = create_jwt_token({"sub": user.username})
-    return {"access_token": jwt_token, "token_type": "bearer"}
+    # if not is_password_correct:
+    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
+    #
+    # jwt_token = create_jwt_token({"sub": user.password})
+    # return {"access_token": jwt_token, "token_type": "bearer"}
